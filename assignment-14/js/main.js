f8e82f549f68f58c8fb033f002688adbcc112f28
Shea Gerard <shea.gerard@outlook.com>
2021-04-21 13:28:25 -0600

    assignment-14 update

--- assignment-14/js/main.js
+++ assignment-14/js/main.js
@@ -1,10 +1,24 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
+ctx.moveTo(100, 100);
+ctx.lineTo(300, 100);
 ctx.stroke();
 
-ctx.moveTo(50, 200);
+ctx.moveTo(300, 100);
+ctx.lineTo(300, 300);
+ctx.stroke();
+
+ctx.moveTo(300, 300);
 ctx.lineTo(100, 300);
+ctx.stroke();
+
+ctx.moveTo(100, 300);
+ctx.lineTo(100, 100);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(200, 100, 40, 0, 2 * Math.PI);
+ctx.fillStyle = "#FF0000";
+ctx.fill();
 ctx.stroke();
